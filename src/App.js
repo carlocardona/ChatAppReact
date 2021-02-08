dcbbdd6f508f06f25d7ee25734813c409037be81
Student <pbjunkiep3@yahoo.com>
2021-02-09 10:06:05 +1100

    created message components and styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <ChatEngine 
+        
+            height="100vh"
+            projectID="1338d8bc-bf2e-4c0d-b73c-9808f3ce5248"
+            userName="admin"
+            userSecret="root"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+        
+        />
+    )
 }
 
 export default App;
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,16 @@
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
+
+const ChatFeed = (props) => {
+    const { chats, activeChat, userName, messages } = props;
+    const chat = chat && chats[activeChat];
+
+    return (
+        <div>
+            ChatFeed
+        </div>
+    );
+}
+
+export default ChatFeed;
