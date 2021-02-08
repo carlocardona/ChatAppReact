b78250afc5f2fc00c02583e69ef096dd95d1bc06
Student <pbjunkiep3@yahoo.com>
2021-02-09 10:28:50 +1100

    added chat title container

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -4,11 +4,43 @@ import TheirMessage from './TheirMessage';
 
 const ChatFeed = (props) => {
     const { chats, activeChat, userName, messages } = props;
+
     const chat = chat && chats[activeChat];
 
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+        return keys.map(( key, index ) => {
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null : keys[index - 1];
+            const isMyMessage = userName === message.sender.username;
+
+            return(
+                <div key={`msg_${index}`} style={{ width: '100%'}}>
+                    <div className="message-block">
+                        {
+                            isMyMessage 
+                            ? <MyMessage />
+                            : <TheirMessage />
+                        }
+                    </div> 
+                    <div className="read-receipts" style={{ margintRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
+                        read receipts 
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    if(!chat) return 'Loading...';
+
     return (
-        <div>
-            ChatFeed
+        <div className="chat-feed">
+            <div className="chat-title-container">
+                <div className="chat-title">{chat.title}</div>
+                <div className="chat-subtitle">
+                    {chat.people.map((person) => ` ${person.perons.username}`)}
+                </div>
+            </div>
         </div>
     );
 }
