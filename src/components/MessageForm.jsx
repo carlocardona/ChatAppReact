7e48c8ccee5eb689872cc73bd08b10a9171fd71a
Student <pbjunkiep3@yahoo.com>
2021-02-09 14:15:43 +1100

    finished send icon photo and send button

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { sendMessage, isTyping } from 'react-chat-engine';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 
 const MessageForm = (props) => {
 
@@ -11,13 +12,18 @@ const MessageForm = (props) => {
 
         const text = value.trim();
 
-        if(text.length > 0) sendMessage(creds, chatId, { text });
+        if(text.length > 0) sendMessage(creds, chatId, { text:''});
+        setValue('');
     }
 
     const handleChange = (e) => {
         setValue(e.target.value);
 
         isTyping( props, chatId );
+    }
+
+    const handleUpload = (e) => {
+        sendMessage( creds, chatId, { files: e.target.value, text: '' })
     }
 
     return(
@@ -29,6 +35,21 @@ const MessageForm = (props) => {
                 onChange={handleChange}
                 onSubmit={handleSubmit}
             />
+            <label htmlFor="upload-button">
+                <span className="image-button">
+                    <PictureOutlined className="picture-icon" />
+                </span>
+            </label>
+            <input 
+                type="file"
+                multiple={false}
+                id="upload-button"
+                style={{ display: 'none' }}
+                onChange={ handleUpload }
+            />
+            <button type="submit" className="send-button">
+                <SendOutlined className="send-icon" />
+            </button>
         </form>
     )
 }
