f971b8d4a3a914f58caf7837f5ac746059ae5813
Student <pbjunkiep3@yahoo.com>
2021-02-09 14:07:59 +1100

    added message text box functionality

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,8 +1,35 @@
-const MessageForm = () => {
+import { useState } from 'react';
+import { sendMessage, isTyping } from 'react-chat-engine';
+
+const MessageForm = (props) => {
+
+    const [value, setValue] = useState('');
+    const { chatId, creds } = props;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const text = value.trim();
+
+        if(text.length > 0) sendMessage(creds, chatId, { text });
+    }
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+
+        isTyping( props, chatId );
+    }
+
     return(
-        <div>
-            Message Form
-        </div>
+        <form className="message-form" onSubmit={handleSubmit} >
+            <input 
+                className="message-input"
+                placeholder="Say Something..."
+                value={value}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+            />
+        </form>
     )
 }
 
